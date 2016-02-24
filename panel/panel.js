dbc6e0f93d766dc5e3fce370c2fab54b43f60051
christinakim <christinahkim1@gmail.com>
2016-02-23 23:00:12 -0600

    added findClearFix

--- panel/panel.js
+++ panel/panel.js
@@ -3,20 +3,98 @@
 
 (function () {
 	console.log("panel.js imported ayeee");
+
+	var getMethodList = function() {
+		var methodList = [];
+	  var CF_DISPLAY = {
+	    name: 'display clearfix',
+	    cssSpec: {
+	      'display': /table-cell|table-caption|inline-block|inline-table/g
+	    }
+	  };
+
+	  var CF_MICRO = {
+	    name: 'micro clearfix',
+	    pseudoEl: [
+	      {
+	        pseudoClass: 'after',
+	        cssSpec: {
+	          'content': /(?:^$)|\w|./,
+	          'display': /block|table/,
+	          'clear': /both/
+	        }
+	      }
+	    ],
+	    cssSpec: {}
+	  };
+
+	  methodList.push(CF_DISPLAY);
+	  methodList.push(CF_MICRO);
+
+	  return methodList;
+	}
+	var methodList = getMethodList();
+
 	// given an element and an array of properties, retrieve their corresponding values
 	// from the element's computed styles
-	var getElementStyleValues = function (el, pseudo, styleKeysArr) {
-		var styleObj = window.getComputedStyle(el, pseudo);
+	
+
+	var getElementStyleValues = function (styleKeysArr, el, pseudo) {
+		var pseudoSelector = pseudo || '';
+		var styleObj = window.getComputedStyle(el, pseudoSelector);
 		var valuesObj = {};
 		var currentStyle;
 
 		for (var i = 0; i < styleKeysArr.length; i++) {
 			currentStyle = styleKeysArr[i];
-			values[currentStyle] = styleObj[currentStyle];
+			valuesObj[currentStyle] = styleObj[currentStyle];
 		}
 
 		return valuesObj;
 	}
+
+	//just for clearfix 
+	var findClearFix = function(methodList) {
+		if (typeof document.getElementsByClassName("clearfix")[0] != typeof undefined){
+			var el = document.getElementsByClassName("clearfix")[0];
+		} else {
+			var el = document.getElementsByClassName("cf")[0];
+		}
+		
+		for (var i = 0; i < methodList.length; i++) {
+			var name = methodList[i]["name"];
+			var keys = 	Object.keys(methodList[i]);
+
+
+
+			if (methodList[i]['pseudoEl']){
+				console.log('did i get here?');
+				var pseudoEl = methodList[i]['pseudoEl'];
+				console.log(pseudoEl);
+				var styleKeys = methodList[i]['cssSpec'];
+				var values = getElementStyleValues(styleKeys, el, pseudoEl);
+				console.log(values[0]);
+			} else {
+				var styleKeys = methodList[i]['cssSpec'];
+				var values = getElementStyleValues(styleKeys, el);
+				console.log(values);
+
+			}
+
+			if (typeof values != typeof undefined) {
+				console.log('found an example of ' + name);
+				console.log(values);
+				return values
+				
+			} else {
+				console.log('nothing found sry try again bye');
+			}
+			
+		}
+
+	}
+
+	findClearFix(methodList);
 
 	// determine whether to include a node based on its
 	// class/id names and/or children
@@ -43,7 +121,7 @@
 		}
 	});
 */
-introJs().start();
+//introJs().start();
 
 	// write higher level functions that include all the ways to check HTML attributes, all the CSS attributes, look through HTML children for structural attributes
 	
